0d016e95ede20c35a51342feb99898bbaea87d17
Olusola1988 <o.afolayan@ucard.store>
2023-01-17 16:29:58 +0100

    amended

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/'  element={<HomePage />}></Route>
+      {/* <Route path='/'  element={<HomePage />}></Route>
       <Route path='/transaction-history' element={<TransactionHistory />} ></Route>
       <Route path='/support' element={<ContactSupport />} ></Route>
-      <Route path='*' element={<NotFound />} />
+      <Route path='*' element={<NotFound />} /> */}
       <Route path='/order-detail-page' element={<OrderDetailPage />}></Route>
     </Routes>
   )
