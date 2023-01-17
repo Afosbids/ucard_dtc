b4a18835863284d4818bc0fff0560d2e74fbc07a
Olusola1988 <o.afolayan@ucard.store>
2023-01-17 16:34:16 +0100

    amended

--- src/pages/OrderDetailPage/OrderDetailPage.jsx
+++ src/pages/OrderDetailPage/OrderDetailPage.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import './OrderDetailPage.css'
-// import OrderDetailNavBar from '../../components/OrderDetailPageComponent/OrderDetailNavBar'
-// import OrderDetailProcess from '../../components/OrderDetailPageComponent/OrderDetailProcess'
-// import OrderDetailHeading from '../../components/OrderDetailPageComponent/OrderDetailHeading'
-// import OrderDetailForm from '../../components/OrderDetailPageComponent/OrderDetailForm'
-// import OrderDetailFooter from '../../components/OrderDetailPageComponent/OrderDetailFooter'
+import OrderDetailNavBar from '../../components/OrderDetailPageComponent/OrderDetailNavBar'
+import OrderDetailProcess from '../../components/OrderDetailPageComponent/OrderDetailProcess'
+import OrderDetailHeading from '../../components/OrderDetailPageComponent/OrderDetailHeading'
+import OrderDetailForm from '../../components/OrderDetailPageComponent/OrderDetailForm'
+import OrderDetailFooter from '../../components/OrderDetailPageComponent/OrderDetailFooter'
 import PaymentOptionDetailModal from '../../components/PaymentOptionDetailModal/PaymentOptionDetailModal'
 
 const OrderDetailPage = () => {
     return (
     <div>
-        {/* <OrderDetailNavBar />
+        <OrderDetailNavBar />
         <OrderDetailProcess />
         <OrderDetailHeading />
         <OrderDetailForm />
-        <OrderDetailFooter /> */}
+        <OrderDetailFooter />
         <PaymentOptionDetailModal />
     </div>
 )
