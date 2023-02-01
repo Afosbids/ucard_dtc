c258d8005e2ed24f443b55a5ca9534289b319d9c
Olusola1988 <o.afolayan@ucard.store>
2023-02-01 10:18:54 +0100

    code amended

--- src/components/OrderDetailPageComponent/OrderDetailForm.jsx
+++ src/components/OrderDetailPageComponent/OrderDetailForm.jsx
@@ -1,144 +1,125 @@
 import React, { useState } from 'react'
-import Select, { selectClasses } from '@mui/joy/Select';
-import Option from '@mui/joy/Option';
-import Avatar from '@mui/joy/Avatar';
-import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import ListDivider from '@mui/joy/ListDivider';
 import './OrderDetailForm.css'
+import indicator from '../../assets/Vector.svg'
 import Dstv_logo from '../../assets/image 12.svg'
 import Gotv_logo from '../../assets/image 13.svg'
 import Startimes_logo from '../../assets/image 14.svg'
 import Showmax_logo from '../../assets/image 15.svg'
-
-// import styled from 'styled-components';
-
-
+import Country_flag from '../../assets/image 6 (1).svg'
+import Search_icon from '../../assets/Search.svg'
 
 const OrderDetailForm = () => {
-    const [selectedOption, setSelectedOption] = useState('');
-    const colorStyles = {
-        control: (styles) => ({...styles, backgroundColor: "white"})
+    const [value, setValue] = useState('');
+    const [value2, setValue2] = useState('');
+    const [optionsVisible, setOptionsVisible] = useState(false);
+    const [optionsVisible2, setOptionsVisible2] = useState(false)
+
+    const toggleOptions = () => {
+        setOptionsVisible(!optionsVisible);
+    };
+
+    const toggleOptions2 = () => {
+        setOptionsVisible2(!optionsVisible2);
     }
 
+    const handleOptionClick = (option) => {
+        setValue(option);
+        setOptionsVisible(false);
+    };
+
+    const handleOptionClick2 = (option) => {
+        setValue2(option);
+        setOptionsVisible2(false)
+    }
     return (
         <div>
-            <section>
-                <form className="form-container">
-                    <div className="form-group">
-                        <label>Select Cable TV Type</label>
-                        <Select
-                        value={selectedOption}
-                        onChange={event => setSelectedOption(event.target.value)}
-                        placeholder="Please select cable tv type"
-                        indicator={<KeyboardArrowDown />}
-                        sx={{
-                            width: 412,
-                            height: 58,
-                            [`& .${selectClasses.indicator}`]: {
-                            // transition: '0.3s',
-                            [`&.${selectClasses.expanded}`]: {
-                            },
-                            },
-                        }}
-                        styles={colorStyles}
-                        >
-                            <Option value="Please select cable tv type"></Option>
-                            <Option>
-                                Dstv
-                            <ListItemDecorator>
-                                <Avatar size="sm" src={Dstv_logo} />
-                            </ListItemDecorator>
-                            </Option>
-                            <ListDivider role="none" inset="startContent" />
-                            <Option>
-                                Gotv
-                            <ListItemDecorator>
-                                <Avatar size="sm" src={Gotv_logo} />
-                            </ListItemDecorator>
-                            </Option>
-                            <ListDivider role="none" inset="startContent" />
-                            <Option>
-                                Startimes
-                            <ListItemDecorator>
-                                <Avatar size="sm" src={Startimes_logo} />
-                            </ListItemDecorator>
-                            </Option>
-                            <ListDivider role="none" inset="startContent" />
-                            <Option>
-                                Showmax
-                            <ListItemDecorator>
-                                <Avatar size="sm" src={Showmax_logo} />
-                            </ListItemDecorator>
-                            </Option>
-                            <ListDivider role="none" inset="startContent" />
-                        </Select><br />
-                        <label>Input Decoder Number</label>
-                        <input type="text" placeholder="Enter your Decoder Number" /><br />
-                        <label>Select Bouquet Type</label>
-                        <Select 
-                        value={selectedOption}
-                        onChange={event => setSelectedOption(event.target.value)}
-                        indicator={<KeyboardArrowDown />}
-                        sx={{
-                                width: 412,
-                                height: 58,
-                            [`& .${selectClasses.indicator}`]: {
-                                // transition: '0.3s',
-                                [`&.${selectClasses.expanded}`]: {
-                                },
-                                },
-                            }}
-                            styles={colorStyles}
-                        >
-                                <Option value=""></Option>
-                                <Option value="DSTV Padi (N2,150)">
-                                <ListItemDecorator>
-                                    DSTV Padi (N2,150 )
-                                </ListItemDecorator>
-                                </Option>
-                                <ListDivider role="none" inset="startContent" />
-                                <Option value="DSTV Yanga (N2,950)">
-                                <ListItemDecorator>
-                                    DSTV Yanga (N2,950)
-                                </ListItemDecorator>
-                                </Option>
-                                <ListDivider role="none" inset="startContent" />
-                                <Option value="DSTV Confam (N5,300)">
-                                <ListItemDecorator>
-                                    DSTV Confam (N5,300)
-                                </ListItemDecorator>
-                                </Option>
-                                <ListDivider role="none" inset="startContent" />
-                                <Option value="DSTV Compact (N9,000)">
-                                <ListItemDecorator>
-                                    DSTV Compact (N9,000)
-                                </ListItemDecorator>
-                                </Option>
-                                <ListDivider role="none" inset="startContent" />
-                                <Option value="DSTV Compact Plus (N14,250)">
-                                <ListItemDecorator>
-                                    DSTV Compact Plus (N14,250)
-                                </ListItemDecorator>
-                                </Option>
-                                <ListDivider role="none" inset="startContent" />
-                                <Option value="DSTV Premium (N21,000)">
-                                <ListItemDecorator>
-                                    DSTV Premium (N21,000)
-                                </ListItemDecorator>
-                                </Option>
-                        </Select><br />
-                        <label>Amount</label>
-                        <input type="text" placeholder="Enter amount" /><br />
-                        <label>Email</label>
-                        <input type="text" placeholder="Enter your email address" /><br />
-                        <label>Select Country</label>
-                        <input type="search" placeholder='Sierra Leone (SL)' /><br />
-                        <button className='btn'>Continue</button>
-                    </div>    
-                </form>
-            </section>
-            
+            <form className='form-container'>
+            <div class="dropdown">
+                <label>Select Cable TV Type</label>
+                <input 
+                type="text"
+                value={value}
+                onClick={toggleOptions}
+                placeholder="Please select cable tv type"
+                className="dropdown-input" />
+                <img src={indicator} class="indicator" onClick={() => handleOptionClick(option)} />
+                {optionsVisible && (
+                        <ul className="dropdown-options">
+                        <li className="dropdown-option" onClick={() => handleOptionClick(option)}>
+                            <p>Dstv</p>
+                            <img src={Dstv_logo} />
+                        </li>
+                        <li className="dropdown-option" onClick={() => handleOptionClick(option)}>
+                            <p>Gotv</p>
+                            <img src={Gotv_logo} />
+                        </li>
+                        <li className="dropdown-option" onClick={() => handleOptionClick(option)}>
+                            <p>Startimes</p>
+                            <img src={Startimes_logo} />
+                        </li>
+                        <li className="dropdown-option" onClick={() => handleOptionClick(option)}>
+                            <p>Showmax</p>
+                            <img src={Showmax_logo} />
+                        </li>
+                    </ul>
+                )}
+            </div>
+            <div class="form-group-1">
+                <label>Input Decoder Number </label>
+                <input type="text" placeholder="Enter your Decoder Number" />
+            </div>
+            <div className="dropdown-2">
+                <label>Select Bouquet Type</label>
+                <input 
+                type="text"
+                value2={value2}
+                onClick={toggleOptions2}
+                className="dropdown-input-2"    
+                    />
+                <img src={indicator} class="indicator-2" onClick={() => handleOptionClick2(option)} />
+                {optionsVisible2 && (
+                    <ul className="dropdown-options-2">
+                        <li className="dropdown-option-2" onClick={() => handleOptionClick2(option)}>
+                            <p>DSTV Padi (N2,150)</p>
+                        </li>
+                        <li className="dropdown-option-2" onClick={() => handleOptionClick2(option)}>
+                            <p>DSTV Yanga (N2,950)</p>
+                        </li>
+                        <li className="dropdown-option-2" onClick={() => handleOptionClick2(option)}>
+                            <p>DSTV Confam (N5,300)</p>
+                        </li>
+                        <li className="dropdown-option-2" onClick={() => handleOptionClick2(option)}>
+                            <p>DSTV Compact (N9,000)</p>
+                        </li>
+                        <li className="dropdown-option-2" onClick={() => handleOptionClick2(option)}>
+                            <p>DSTV Compact Plus (N14,250)</p>
+                        </li>
+                        <li className="dropdown-option-2" onClick={() => handleOptionClick(option)}>
+                            <p>DSTV Premium (N21,000)</p>
+                        </li>
+                    </ul>
+                )}
+            </div>
+            <div class="form-group-2">
+                <label>Amount </label>
+                <input type="text" placeholder="Enter amount" />
+            </div>
+            <div class="form-group-3">
+                <label>Email </label>
+                <input type="text" placeholder="Enter your valid email address" />
+            </div>
+            <div class="form-group-4">
+                <label>Select Country </label>
+                <input type="text" placeholder="Sierra Leone (SL)" />
+                <div className="form-group-4-img">
+                    <img src={Country_flag} className="img-1" />
+                    <img src={Search_icon} />
+                </div>
+            </div>
+            <div className="form-btn">
+                <input type="button" value="Continue" />
+            </div>
+            </form>
         </div>
     )
 }
