0140d619fabcad6b6ab2ed7dd34bf76d153b4ee5
Olusola1988 <o.afolayan@ucard.store>
2023-02-08 21:23:43 +0100

    transaction history page amended

--- src/pages/TransactionHistoryPage/TransactionHistoryPage.jsx
+++ src/pages/TransactionHistoryPage/TransactionHistoryPage.jsx
@@ -1,35 +1,43 @@
 import React, { useState } from 'react'
-import OrderDetailNavBar from '../../components/OrderDetailPageComponent/OrderDetailNavBar'
 import TransactionSuccessfulModal from '../../components/TransactionSuccessfulModal/TransactionSuccessfulModal'
 import './TransactionHistoryPage.css'
 
 const TransactionHistoryPage = () => {
     const [showModal, setShowModal] = useState(false)
-
-        const openModal = () => {
-            setShowModal(true);
-        };
-    
-        const closeModal = () => {
-            setShowModal(false);
-        };
     return (
         <div>
-            <div className="transaction_header">
-                <h1>Transaction History </h1>
-                <p>See history of your payments and receipts. Complete the following details to generate your transaction history </p>
-            </div>
-            <div className="transaction_input">
-                <p>Decoder/ Smartcard Number </p>
-                <input type="text" placeholder="Input your decoder number to generate history " />
-            </div>
-            <div className='transaction_btn'>
-                <button onClick={openModal}>Submit</button>
-                <TransactionSuccessfulModal showModal={showModal} closeModal={closeModal} />
-            </div>
+                <div className="transaction_header">
+                    <h1>Transaction History </h1>
+                    <p>See history of your payments and receipts. Complete the following details to generate your transaction history </p>
+                </div>
+                <div className="transaction_input">
+                    <p>Decoder/ Smartcard Number </p>
+                    <input type="text" placeholder="Input your decoder number to generate history " />
+                </div>
+                <div className='transaction_btn'>
+                    <button onClick={() => setShowModal(true)}>Submit</button>
+                    {showModal && (
+                        <div onClick={() => setShowModal(false)}>
+                            <Modal>
+                                <TransactionSuccessfulModal showModal={showModal} />
+                            </Modal>
+                        </div>
+                    )}
+                </div>
         </div>
 
     )
 }
+
+
+function Modal({ children }) {
+    return (
+        <div style={{ background: 'rgba(0, 0, 0, 0.5)', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0 }}>
+            <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '20px' }}>
+            {children}
+            </div>
+        </div>
+        );
+    }
 
 export default TransactionHistoryPage
