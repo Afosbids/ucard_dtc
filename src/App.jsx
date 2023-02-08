b5c9f0cb45997f20b85ec86004bcb1ac7a881d69
Olusola1988 <o.afolayan@ucard.store>
2023-02-08 23:06:09 +0100

    code amended

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,9 @@ import ContactSupport from './pages/ContactSupport/ContactSupport'
 import OrderDetailPage from './pages/OrderDetailPage/OrderDetailPage'
 import WrongOrderDetailPage from './pages/WrongOrderDetailPage/WrongOrderDetailPage'
 import OrderSummaryPage from './pages/OrderSummaryPage/OrderSummaryPage'
-// import PaymentOptionDetailPage from './pages/PaymentOptionDetailPage/PaymentOptionDetail'
 import PaymentOrderDetailPage from './pages/PaymentOrderDetailPage/PaymentOrderDetail'
 import TransactionConfirmPage from './pages/TransactionConfirmPage/TransactionConfirmPage'
 import TransactionHistoryPage from './pages/TransactionHistoryPage/TransactionHistoryPage'
-import TransactionSuccessfulModal from './components/TransactionSuccessfulModal/TransactionSuccessfulModal'
 import { Layout } from './components/Layout/Layout'
 import SelectPaymentOption from './components/SelectPaymentOption/SelectPaymentOption'
 import StepperForm from './pages/StepperForm/StepperForm'
@@ -28,14 +26,11 @@ function App() {
       <Route path='/order-detail-page' element={<OrderDetailPage />}></Route>
       <Route path='/wrong-order-detail-page' element={<WrongOrderDetailPage />}></Route>
       <Route path='/order-summary-page' element={<OrderSummaryPage />}></Route>
-      {/* <Route path='/payment-option-detail-page' element={<PaymentOptionDetailPage />}></Route> */}
       <Route path='/payment-order-detail-page' element={<PaymentOrderDetailPage />}></Route>
       <Route path='/transaction-confirm-page' element={<TransactionConfirmPage />}></Route>
       <Route path='/transaction-history-page' element={<TransactionHistoryPage />}></Route>
       <Route path='/select-payment-option' element={<SelectPaymentOption />}></Route>
       <Route path='/stepper-form' element={<StepperForm />}></Route>
-      <Route path='/transaction-success' element={<TransactionSuccessfulModal />}></Route>
-      {/* <Route path='/dropdown-bouquet' element={<DropdownBouquet />}></Route> */}
       <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
